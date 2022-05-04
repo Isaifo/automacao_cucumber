de389769e04d7548daf2e583c955eb01becc22d9
Isadora Fernandes Oliveira <isadora.fernandes@inmetrics.com.br>
2022-05-04 15:26:46 -0300

    Add arquivos

---
+++ src/test/java/LearnCucumber.java
@@ -0,0 +1,28 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.it.Quando;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+
+public class LearnCucumber {
+	
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Quando("^executa lo$")
+	public void executa_lo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Entao("^a especificacao deve finalizar com sucesso$")
+	public void a_especificacao_deve_finalizar_com_sucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+}
+
+
+
